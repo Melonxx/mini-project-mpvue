43d70ccf8129100ea99c59d7a7cd82bb42e63682
zhangchao <529743595@qq.com>
2019-06-28 09:13:29 +0800

    2019年6月28日 09:13:20 init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>我的小程序</h1>
+    <h1>{{motto}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme'
+    }
+  },
+  methods: {
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    }
+  },
+  created () {
+    console.log(getApp())
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
